31750175f36efae4fe4ab9e894d03d512ca9c5ac
Drowze <gibim6@gmail.com>
2017-12-30 20:56:01 -0200

    More organization. More packages

--- chroot.sh
+++ chroot.sh
@@ -11,17 +11,7 @@ USER_LOCALE="pt_BR.UTF-8 UTF-8"
 LANG_VARIABLE=pt_BR.UTF-8
 KEYBOARD=br-abnt2
 
-grub-install --target=i386-pc --recheck /dev/sda
-sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=1/g' /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg
-
-systemctl enable sshd.service
-systemctl enable dhcpcd.service
-systemctl enable ntpd.service
-
-echo -e 'EDITOR=vim' > /etc/environment
-echo -e 'runtime: archlinux.vim\nsyntax on' > /etc/skel/.vimrc
-
+# Timezone, locale and keyboard settings
 ln -f -s /usr/share/zoneinfo/$TIMEZONE /etc/localtime
 hwclock --systohc --utc
 echo $USER_LOCALE > /etc/locale.gen
@@ -29,11 +19,27 @@ locale-gen
 echo "LANG=$LANG_VARIABLE" >> /etc/locale.conf
 echo "KEYMAP=$KEYBOARD" >> /etc/vconsole.conf
 
+# Services
+systemctl enable sshd.service
+systemctl enable dhcpcd.service
+systemctl enable ntpd.service
+systemctl enable vboxservice.service
+
+# misc 
+echo -e 'EDITOR=vim' > /etc/environment
+echo -e 'runtime: archlinux.vim\nsyntax on' > /etc/skel/.vimrc
+
+# Network huh
 echo $HOSTNAME > /etc/hostname
 useradd -m -G wheel -s /bin/bash $USERNAME
-
 echo 'name_servers="8.8.8.8 8.8.4.4"' >> /etc/resolvconf.conf
 
+# bootloader
+grub-install --target=i386-pc --recheck /dev/sda
+sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=1/g' /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# "SECURITY"
 echo "$USERNAME:$PASSWORD" | chpasswd
 echo "root:$ROOT_PASSWORD" | chpasswd
 echo "FINISHED"
--- run.sh
+++ run.sh
@@ -13,7 +13,9 @@ pacman -Syy
 mkdir -p /mnt$SCRIPT_DIR
 mv ./chroot.sh /mnt$SCRIPT_DIR
 
-pacstrap /mnt base base-devel grub openssh sudo ntp
+pacstrap /mnt base base-devel grub openssh sudo ntp exa \
+              fish tmux xsel git neovim htop virtualbox-guest-utils \
+
 genfstab -U /mnt > /mnt/etc/fstab
 arch-chroot /mnt /bin/bash $SCRIPT_DIR/chroot.sh
 
