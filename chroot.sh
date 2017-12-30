8c3f5deeaead7725d2b9428fe5596c9ccab8adfa
Drowze <gibim6@gmail.com>
2017-12-30 19:17:13 -0200

    Fix password attribution sequence

--- chroot.sh
+++ chroot.sh
@@ -8,6 +8,8 @@ PASSWORD=$PASSWORD
 ROOT_PASSWORD=$PASSWORD
 TIMEZONE=America/Sao_Paulo
 USER_LOCALE="pt_BR.UTF-8 UTF-8"
+LANG_VARIABLE=pt_BR.UTF-8
+KEYBOARD=br-abnt2
 
 grub-install --target=i386-pc --recheck /dev/sda
 sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=1/g' /etc/default/grub
@@ -23,13 +25,16 @@ echo -e 'runtime: archlinux.vim\nsyntax on' > /etc/skel/.vimrc
 echo $HOSTNAME > /etc/hostname
 useradd -m -G wheel -s /bin/bash $USERNAME 
 
-echo "$USERNAME:$PASSWORD" | chpasswd
-echo "root:$ROOT_PASSWORD" | chpasswd
-
 ln -f -s /usr/share/zoneinfo/$TIMEZONE /etc/localtime
 hwclock --systohc --utc
 echo $USER_LOCALE > /etc/locale.gen
 locale-gen
+LANG=$LANG_VARIABLE
+localectl set-keymap --no-convert $KEYBOARD
 
 echo 'name_servers="8.8.8.8 8.8.4.4"' >> /etc/resolvconf.conf
+
+echo "$USERNAME:$PASSWORD" | chpasswd
+echo "root:$ROOT_PASSWORD" | chpasswd
+echo "FINISHED"
 
